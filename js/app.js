fca36bfabafa8e0bc22d33c8b1705bb84b212286
Rachel Varon <rkvaron@gmail.com>
2018-06-15 22:59:30 -0600

    Changes to dropdown

--- js/app.js
+++ js/app.js
@@ -7,42 +7,34 @@ alertButton.addEventListener('click', ()=> {
   alertDiv.style.display = 'none';
 });
 
+/* Notification drop down menu click on and off*/
 
-/* Notification icon on bell in header- green alert dot
+const notificationAlert = document.querySelector('.notification'); //parent div of myDropdown
+let newNotification = 2;
+const bell = document.getElementById('bell'); //svg only
+const dropdown = document.getElementById('myDropdown');
+const triangle = document.getElementById('triangle');
+
+/* Notification icon on bell in header
 Exceeds: display at least 2 notifications when the user clicks the alert icon*/
 
-const notificationAlert = document.querySelector('.notification');
-let newNotification = 2;
-const bell = document.getElementById('bell');
-const notificationMenu = document.getElementById('notificationMenu');
 
-/*add a green alert if new notifications exist */
-function setNotificationAlert() {
-  if (newNotification > 0) {
-    newNotification.style.opacity = 100;
-  } else {
-    newNotification.style.opacity = 0;
-  }
+function toggleNotifications() {
+    dropdown.classList.toggle("show");
+    triangle.classList.toggle("show");
 }
 
 bell.addEventListener('click', ()=> {
-  if (notificationMenu.classList.length == 0) {
-    notificationMenu.classList.add("notification-menu");
-}
-/*needs to add children in here with the notification info instead of in the html*/
+  toggleNotifications();
 });
 
+/* timezone change color of selection */
+const timezone = document.getElementById('timezone');
 
+function changeTimezoneColor() {
+  timezone.style.color = '#555';
+}
 
-// var el = document.querySelector('.notification');
-//
-// document.querySelector('button').addEventListener('click', function(){
-//     var count = Number(el.getAttribute('data-count')) || 2;
-//     el.setAttribute('data-count', count + 1);
-//     el.classList.remove('notify');
-//     el.offsetWidth = el.offsetWidth;
-//     el.classList.add('notify');
-//     if(count === 0){
-//         el.classList.add('show-count');
-//     }
-// }, false);
+timezone.addEventListener('click', ()=> {
+  changeTimezoneColor();
+});

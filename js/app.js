a86440ddd9a923a885e82a05aa94f28878cf6c07
Rachel Varon <rkvaron@gmail.com>
2019-01-29 10:20:46 -0500

    Small changes to improve code

--- js/app.js
+++ js/app.js
@@ -15,18 +15,27 @@ const bell = document.getElementById('bell'); //svg only
 const dropdown = document.getElementById('myDropdown');
 const triangle = document.getElementById('triangle');
 
+
 /* Notification icon on bell in header
 Exceeds: display at least 2 notifications when the user clicks the alert icon*/
 
 
-function toggleNotifications() {
-    dropdown.classList.toggle("show");
-    triangle.classList.toggle("show");
+function showNotifications() {
+    dropdown.classList.add("show");
+    triangle.classList.add("show");
 }
 
 bell.addEventListener('click', ()=> {
-  toggleNotifications();
+  showNotifications();
 });
+
+
+
+notificationAlert.addEventListener('mouseleave', () => {
+        dropdown.classList.remove("show");
+        triangle.classList.remove("show");
+      });
+
 
 /* timezone change color of selection */
 const timezone = document.getElementById('timezone');
@@ -41,18 +50,44 @@ timezone.addEventListener('click', ()=> {
 
 /* Nav menu selected item changes when clicked */
 
-/* Form Validation for messageUser */
 
-/* onSubmit messageUser form gives confirmation message to user */
+
+/* setting variables for the Message User form */
 
 const messageform = document.getElementById('messageUser');
 const submitP = document.getElementById('submit_message');
 const input = document.getElementById('user_search');
 const textarea = document.getElementById('comments');
+const messageButton = document.getElementById('messageButton');
+
+
+
+/* Form Validation for messageUser */
+
+function validateForm(e) {
+    
+        // add tooltip here?? 
+        //<span class="tool" data-tip="Please enter the user's name." tabindex="1"></span>
+    }
+
+
+
+
+/* onSubmit messageUser form gives confirmation message to user */
+
 
 messageform.addEventListener('submit', (e) => {
-  e.preventDefault();
+  let x = input.value;
+    let y = textarea.value;
+    e.preventDefault();
+    if (x || y == "") {
+      console.log("missing input");
+      //show tooltip to add in an input
+    } else {
+
   input.value = '';
   textarea.value = '';
   submitP.innerHTML = "Your message has been sent.";
+    }
+  
 });
